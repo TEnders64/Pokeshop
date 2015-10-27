601c188918adb423582fe4bf3054c973a5b0c988
Geneva Le <genevahdle@yahoo.com>
2015-10-27 13:42:46 -0700

    last time i thought i pushed but i didnt

--- application/controllers/admins.php
+++ application/controllers/admins.php
@@ -4,7 +4,20 @@ class Admins extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('adminslogins');
+		$this->load->view('adminlogins');
+	}
+	public function login(){
+		// var_dump($this->input->post());
+		// die();
+		if($this->admin->login($this->input->post())){
+			redirect("/admins/adminorders");
+		}
+		else{
+			redirect("/admins/index");
+		}
+	}
+	public function adminorders(){
+		$this->load->view('adminorders');
 	}
 }
 ?>
