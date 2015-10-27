522f86dd4df0458d88c83388dbfa2e021e9f0eab
Geneva Le <genevahdle@yahoo.com>
2015-10-27 10:57:31 -0700

    Added controllers, views, did autoload models, and autoloaded libraries, and changed default route

---
+++ application/views/show.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>PokeProducts</title>
+	<link rel="stylesheet" type="text/css" href="/assets/style.css">
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+
+		}) //end of document JQuery 
+	</script><!-- end script -->
+</head><!-- end head -->
+<body>
+	<div id="container">
+		<div id="header"></div> <!-- end Header -->
+		<div id="sidePanel">
+			<h1>Bulbasaur</h1>
+                <img src="http://assets12.pokemon.com/assets/cms2/img/pokedex/detail/001.png" alt="bulbasaur" >
+            
+		</div> <!-- end SidePanel -->
+		<div id="mainContent">
+			<div id="textBox">
+				<p>dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext </p>
+				<p>Weight:</p>
+				<p>Height:</p>
+				<p>Type:</p>
+				<p>Price: </p>				
+			</div>
+			<form>
+				<input type="hidden">
+				<input type="number" name="quantity" min="1" max="10">
+				<input type="submit" value="BUY">
+			</form>
+		</div> <!-- end MainContent -->
+		<div id="similar">
+			<h2>Similar Pokemons:</h2>
+			<div id="similarPoke">
+				<img src="http://assets12.pokemon.com/assets/cms2/img/pokedex/detail/002.png" alt="venusaur">
+				<img src="http://assets12.pokemon.com/assets/cms2/img/pokedex/detail/003.png" alt="ivysaur">
+			</div>
+		</div> <!-- end Similar -->
+	</div> <!-- end container -->
+</body><!-- end body -->
+</html>
