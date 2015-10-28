e4fcaf5b4e976b716769925795269bb05f598133
weetabixs <blake.v.lamb@gmail.com>
2015-10-28 10:30:51 -0700

    changes to products page
    adding support for getting data

--- application/models/customer.php
+++ application/models/customer.php
@@ -2,6 +2,12 @@
 
 class Customer extends CI_Model {
 
+	public function all_pokemon($post){
+		$query = "SELECT id, name FROM pokemons";
+		$values = $post['all_pokemon'];
+		return $this->db->query($query, $values)->result_array();
+	}
+
 
 }
 ?>
